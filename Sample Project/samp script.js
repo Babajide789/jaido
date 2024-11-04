1b4ba82f86bc77d90d260d68459e5baf70638e2d
Babajide789 <102975274+Babajide789@users.noreply.github.com>
2024-11-04 15:26:55 -0500

    Add files via upload

---
+++ Sample Project/samp script.js
@@ -0,0 +1,14 @@
+const hamburger = document.querySelector(".hamburger")
+
+const navLinks = document.querySelector(".nav-links")
+
+// CLICK EVENT
+
+// MOUSE HOVER EVENNT
+
+// MOUSE LEAVE EVENT
+
+hamburger.addEventListener('click', ()=>{
+    navLinks.classList.toggle("active")
+    
+})
